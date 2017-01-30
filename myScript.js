ed84711fa5f8c4cd888a2703853e349f0fe67878
Yev Rapoport <yev.rap@gmail.com>
2017-01-30 15:35:43 -0500

    Adding win timer

--- myScript.js
+++ myScript.js
@@ -1,16 +1,30 @@
 $(document).ready(function(){
     
+	$("#start").click(function(){
+		$("img").css("visibility", "visible");
+		startTime = new Date;
+		/*
+		var start = new Date;
+		setInterval(function() {
+			$('.timer').text(Math.round((new Date - start) / 1000, 0) + " Seconds");
+		}, 1000);
+		*/
+		
+	});
+	
     $("#reset").click(function(){
+		$("#reset").css("visibility", "hidden")
         $("img").removeClass("wrong right");
 		$("p").empty();
     });
     
     $("img").click(function() {
-        borderColor($(this))
-		winText()  
+        setBorderColor($(this));
+		$("#reset").css("visibility", "visible")
+		winText();
     });
 	
-	function borderColor(image){
+	function setBorderColor(image){
 		if(image.attr("class").includes("cat") == true){
             image.toggleClass("right");
         } else {
@@ -19,15 +33,16 @@ $(document).ready(function(){
 	}
 	
 	function winText(){
-		var numOfCats = $(".catImg").length
-        var catsSelected = $(".right").length
-        var otherSelected = $(".wrong").length
+		var numOfCats = $(".catImg").length;
+        var catsSelected = $(".right").length;
+        var otherSelected = $(".wrong").length;
 		
 		if(catsSelected==numOfCats && otherSelected==0) {
-            $("p").text("You found them all!");
+			endTime = new Date;
+			var gameTime = Math.round((endTime - startTime) / 1000, 0);
+            $("p").text("You found them all in " + gameTime + " seconds");
         } else {
             $("p").empty();
         }
 	}
-	
 });
