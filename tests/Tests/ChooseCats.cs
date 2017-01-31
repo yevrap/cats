da9cacc5bd6bb2be64cabbf82da6e31c235f8a58
Yev Rapoport <yev.rap@gmail.com>
2017-01-31 14:50:29 -0500

    Adding selenium test project

---
+++ tests/Tests/ChooseCats.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+
+namespace Tests
+{
+    [TestClass]
+    public class ChooseCats
+    {
+
+        IWebDriver driver;
+        string url = "https://yevrap.github.io/cats";
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(url);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void PageLoad()
+        {
+            string title = driver.Title;
+            IWebElement startButton = driver.FindElement(By.Id("start"));
+            Assert.AreEqual(title, "Cats");
+            Assert.AreEqual(startButton.GetAttribute("id"), "start");
+        }
+    }
+}
